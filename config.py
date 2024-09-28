0a7055ec797be50dc6ae5804c581dfb1172ba8db
iandinoo <nuriadeviana@gmail.com>
2024-09-29 05:43:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -69,7 +69,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((1494610306))
+ADMINS.extend(())
 
 
 LOG_FILE_NAME = "logs.txt"
